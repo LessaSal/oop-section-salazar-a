4d7be85fb671683fba12d918509819c74d2be9b8
lessa.sal <alessa.salazar.bvt2023@gmail.com>
2023-12-08 13:46:49 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function Player(name) {
+  this.name = name;
+  this.lvl = 1;
+  this.points = 0;
+}
+
+Player.prototype.gainXP = function (xp) {
+  this.points += xp;
+
+  if (this.points >= 10) {
+    this.lvl++;
+    this.points -= 10;
+  }
+
+  console.log(this.describe());
+}
+
+Player.prototype.describe = function () {
+  return `${this.name} is level ${this.lvl} with ${this.points} experience points`;
+}
+
+const player1 = new Player("Belen");
+const player2 = new Player("Alessa");
+
+player1.gainXP(4);
+player2.gainXP(8);
+player1.gainXP(2);
+player2.gainXP(9);
+player1.gainXP(5);
+player2.gainXP(3);
+
+
